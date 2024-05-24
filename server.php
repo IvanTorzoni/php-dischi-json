06b64ab9970d4db17b93dd8d872ad6a970d52055
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-05-24 11:23:01 +0200

    feat: added card in page

--- server.php
+++ server.php
@@ -1,3 +1,21 @@
 <?php
+// Prendo il contenuto dal file json
+$list_dischi_json = file_get_contents("dischi.json");
 
+// Converto il contenuto per creare successivamente un array associativo
+$list_php = json_decode($list_dischi_json, true);
+
+// Creo un array associativo
+$array = [
+    "results" => $list_php
+];
+
+// Trasformo in stringa json
+$json_string = json_encode($array);
+
+// Passo il contenuto a javascript
+header("Content-Type: application/json");
+
+// Output
+echo $json_string;
 ?>
